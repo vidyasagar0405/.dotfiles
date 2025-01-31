3ee69c8cd17e73cd264bebb591d734bba33f82c4
vidyasagar0405 <vidyasagar0405@gmail.com>
2025-01-31 23:28:34 +0530

    added remove trailing white spaces

--- mynvim/.config/mynvim/lua/vs/config/autocommands.lua
+++ mynvim/.config/mynvim/lua/vs/config/autocommands.lua
@@ -108,3 +108,12 @@ vim.api.nvim_create_autocmd("BufEnter", {
 		vim.opt.colorcolumn = "80"
 	end,
 })
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    pattern = {"*"},
+    callback = function()
+      local save_cursor = vim.fn.getpos(".")
+      pcall(function() vim.cmd [[%s/\s\+$//e]] end)
+      vim.fn.setpos(".", save_cursor)
+    end,
+})
