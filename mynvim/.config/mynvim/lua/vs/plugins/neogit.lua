f39bac6f6a023ceb27ad99ec17d989179f34bae7
vidyasagar0405 <vidyasagar0405@gmail.com>
2025-02-06 18:44:44 +0530

    feat(nvim): added keymaps to neogit

--- mynvim/.config/mynvim/lua/vs/plugins/neogit.lua
+++ mynvim/.config/mynvim/lua/vs/plugins/neogit.lua
@@ -1,14 +1,23 @@
 return {
-  "NeogitOrg/neogit",
-  dependencies = {
-    "nvim-lua/plenary.nvim",         -- required
-    "sindrets/diffview.nvim",        -- optional - Diff integration
+    "NeogitOrg/neogit",
+    dependencies = {
+        "nvim-lua/plenary.nvim",         -- required
+        "sindrets/diffview.nvim",        -- optional - Diff integration
 
-    -- Only one of these is needed.
-    "nvim-telescope/telescope.nvim", -- optional
-    -- "ibhagwan/fzf-lua",              -- optional
-    -- "echasnovski/mini.pick",         -- optional
-  },
-  config = true
+        -- Only one of these is needed.
+        "nvim-telescope/telescope.nvim", -- optional
+        -- "ibhagwan/fzf-lua",              -- optional
+        -- "echasnovski/mini.pick",         -- optional
+    },
+    config = function()
+
+        local neogit = require "neogit"
+        vim.keymap.set("n", "<leader>gg", function() neogit.open() end, { desc = "Open Neo[g]it" })
+        vim.keymap.set("n", "<leader>gG", function() neogit.open({ kind = "split" }) end, { desc = "Open Neo[g]it in Split" })
+        vim.keymap.set("n", "<leader>gc", function() neogit.open({"commit"}) end, { desc = "Neogit commit" })
+        vim.keymap.set("n", "<leader>gP", function() neogit.open({"push"}) end, { desc = "Neogit Push" })
+        vim.keymap.set("n", "<leader>gp", function() neogit.open({"push"}) end, { desc = "Neogit pull" })
+
+    end
 }
 
